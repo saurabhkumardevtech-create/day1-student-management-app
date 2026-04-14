7a9681a88a939ac36c04f5739edc1caa3fbf1e1b
Sourav008 <kumarsaurabh06870@gmail.com>
2026-04-14 15:28:16 +0530

    Day 1 React Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [students, setStudents] = useState([]);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+    course: ""
+  });
+
+  // Input handle
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // Submit data
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStudents([...students, formData]);
+
+    setFormData({
+      name: "",
+      age: "",
+      course: ""
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1>Student Management App</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <br /><br />
+
+        <input
+          type="number"
+          name="age"
+          placeholder="Enter Age"
+          value={formData.age}
+          onChange={handleChange}
+        />
+        <br /><br />
+
+        <input
+          type="text"
+          name="course"
+          placeholder="Enter Course"
+          value={formData.course}
+          onChange={handleChange}
+        />
+        <br /><br />
+
+        <button type="submit">Add Student</button>
+      </form>
+
+      <h2>Student List</h2>
+
+      <ul>
+        {students.map((s, index) => (
+          <li key={index}>
+            {s.name} | {s.age} | {s.course}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
